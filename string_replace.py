82e3b13bb34a7e05571308015649110065e8620d
f0kes <f0kesppokes@gmail.com>
2024-06-21 20:54:53 +0300

    +

--- string_replace.py
+++ string_replace.py
@@ -12,12 +12,21 @@ class InputStringConstructor:
                 }),
                 "model_folder": ("STRING", {
                     "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
-                    "default": "diffuseWOLK/"
+                    "default": "wolf"
+                }),
+                "sub_folder" : ("STRING", {
+                    "multiline": False,
+                    "default": "diffuse"
+                }),
+                "animation_name" : ("STRING", {
+                    "multiline": False,
+                    "default": "walk"
                 }),
                 "rotation" : ("STRING", {
                     "multiline": False,
                     "default": "0"
                 }),
+                "include_sub_folder": (["enable", "disable"],),
             },
         }
 
@@ -30,13 +39,17 @@ class InputStringConstructor:
 
     CATEGORY = "String Constructor"
 
-    def dojob(self, base_folder, model_folder, rotation):
+    def dojob(self, base_folder, model_folder, sub_folder, animation_name, rotation, include_sub_folder):
         print(f"""Your input contains:
             base_folder: {base_folder}
             model_folder: {model_folder}
+            animation_name: {animation_name}
             rotation: {rotation}
         """)
-        return (base_folder + model_folder + rotation,)
+        if include_sub_folder == "enable":
+            return (base_folder + model_folder + "/" + sub_folder + "/" + animation_name+ " / " + rotation,)
+        else:
+            return (base_folder + model_folder + "/" +animation_name+ " / " + rotation,)
 
     #@classmethod
     #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
