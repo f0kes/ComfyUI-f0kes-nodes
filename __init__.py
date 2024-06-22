2a28ccf4acbffdf1b95ad7caf949d53b46186417
f0kes <f0kesppokes@gmail.com>
2024-06-22 13:07:41 +0300

    +

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from string_combinations import StringCombinations
 from .string_replace import InputStringConstructor
 from .split_by_new_line import StringSplitter, UnzipStrings
 from .number_of_files_in_dir import FileCounter
@@ -6,7 +7,8 @@ NODE_CLASS_MAPPINGS = {
     "InputStringConstructor": InputStringConstructor,
     "StringSplitter": StringSplitter,
     "UnzipStrings": UnzipStrings,
-    "FileCounter": FileCounter
+    "FileCounter": FileCounter,
+    "StringCombinations": StringCombinations
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -14,6 +16,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "InputStringConstructor": "Input String Constructor Node",
     "StringSplitter": "String Splitter Node",
     "UnzipStrings": "Unzip Strings Node",
-    "FileCounter": "File Counter Node"
+    "FileCounter": "File Counter Node",
+    "StringCombinations": "String Combinations Node"
 }
-__all__ = ["InputStringConstructor", "StringSplitter", "UnzipStrings", "FileCounter"]
+__all__ = ["InputStringConstructor", "StringSplitter", "UnzipStrings", "FileCounter", "StringCombinations"]
---
+++ string_combinations.py
@@ -0,0 +1,33 @@
+class StringCombinations:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "first_zipped": ("ZIPPED_STRING",), 
+                "second_zipped": ("ZIPPED_STRING",), 
+            },
+        }
+
+    RETURN_TYPES = ("STRING","STRING",)
+    RETURN_NAMES = ("first string", "second string",)
+
+    FUNCTION = "dojob"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "String Constructor"
+
+    def dojob(self, first_zipped, second_zipped):
+         # Create all combinations of pairs from the two lists
+        combinations = [(a, b) for a in first_zipped for b in second_zipped]
+        first_elements = [a for a, b in combinations]
+        second_elements = [b for a, b in combinations]
+        return (first_elements, second_elements)
+
+    #@classmethod
+    #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
+    #    return ""
+
